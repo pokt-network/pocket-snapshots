39e48916b48c4841b49486818a67f20c83ec80d3
uploaderbot <uploaderbot@pokt.network>
2021-11-09 15:41:42 +0000

    update snapshot script

--- refresh-script.sh
+++ refresh-script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 export PATH=/usr/local/sbin:/sbin:/bin:/usr/sbin:/usr/bin:/root/bin:/usr/local/bin/
-export UPLINK_WRITE=YOUR_UPLINK_WRITE_KEY
+export UPLINK_WRITE=<YOUR_WRITE_PERM_KEY>
 export UPLINK_DOWN=147A7s3UVY6g4DhxdatsM7QMofNBJJfvcq5w9XuYjU2HrmEbr4JSbRy3NQu3mijqk7T8in1PYEAdcf11dd5yhJ4eDAn4UMppBgqcN49f2tHVcGhRV2McpvyTm4U22uXH35h14JA1YXiGdUFDss7ThTnFnPYY8uRTxmtG2UrdW9LZkmuJysNF1sU8anEGcZnGQuYWViAzVx2VwtYTrYQE5CXPQotB2rnGwFaUY9vVeTCKFC8yiwZLHxhPJdZaexrZPbBTaf1xvmuyarMchkxvbn8K7pLXfw7n2xGArJavvRK86Nj1SrRr5ws9ku9i24WbGddKWz4SNaZgUH63Wm65yK8m91kgeHLDhhhR
 
 docker stop node11
@@ -37,7 +37,6 @@ git clone git@github.com:pokt-network/pocket-snapshots.git
 cd pocket-snapshots
 
 tarlink=$(uplink share --not-after=none --url --access "$UPLINK_WRITE"  sj://pocket-public-blockchains/"$tarname" |  grep "URL  " | awk  '{print $3}')
-#sed -i -e 's,https:\(.*\)\(.tar\|.tar $\),'"$tarlink"',g' README.md
 
 
 compressedlink=$(uplink share --not-after=none --url --access "$UPLINK_WRITE"  sj://pocket-public-blockchains/"$compressedname"  |  grep "URL  " | awk  '{print $3}')
@@ -55,4 +54,3 @@ git push origin master
 rm -rf ~/pocket-snapshots
 echo "snapshot $name done" >> /root/snapshot-log.txt
 date >> /root/snapshot-log.txt
-#uplink cp --access uploaderbot  sj://pocket-public-blockchains/$name sj://pocket-public-blockchains/pocket-network-data-RC-0.6.3.6.tar --config-dir /root/.local/share/storj/uplink
